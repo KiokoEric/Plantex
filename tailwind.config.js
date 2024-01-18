a96de6ae4b2e92ab188daf7d1599c88ac404cacd
“[KiokoEric]” <[kiokoerick040@gmail.com]>
2024-01-18 22:59:39 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./Build/index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        Instrument: ["Instrument Serif"],
+        Playfair: ["Playfair Display"]
+      }
+    },
+  },
+  plugins: [],
+}
+
